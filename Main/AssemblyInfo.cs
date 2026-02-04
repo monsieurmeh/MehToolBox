c1fe8c6e4c19365f70e794a92dc66986ab091216
MonsieurMeh <mensoirmeh@gmail.com>
2026-02-04 10:12:15 -0800

    init

---
+++ Main/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyTitle("MehToolBox")]
+[assembly: AssemblyCopyright("MonsiuerMeh")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(MehToolBox.Main), "MehToolBox", "1.0.0", "MonsieurMeh", null)]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
---
+++ Main/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System.Reflection;
+using Il2Cpp;
+
+
+namespace MehToolBox
+{
+    internal class Main : MelonMod
+    {
+        //Nothing to see here, just a utility melon loading in utilities...
+    }
+}
