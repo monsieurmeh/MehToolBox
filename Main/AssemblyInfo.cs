03890e7a6b993bb093e2a6fb95c3f51c6a472986
MonsieurMeh <mensoirmeh@gmail.com>
2026-02-14 00:29:51 -0800

    1.1.0

--- Main/AssemblyInfo.cs
+++ Main/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("MehToolBox")]
 [assembly: AssemblyCopyright("MonsiuerMeh")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(MehToolBox.Main), "MehToolBox", "1.0.0", "MonsieurMeh", null)]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: MelonInfo(typeof(MehToolBox.Main), "MehToolBox", "1.1.0", "MonsieurMeh", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
